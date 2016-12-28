07423f2e13f87f5731ecc44b63b236c9f6d13f41
Eric PC <eric.hoog88@gmail.com>
2016-12-27 19:23:19 -0500

    Initial Commit
    There's a Sphere that you can control with the keyboard.  AAA game

---
+++ Assets/Scripts/MovementControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControls : MonoBehaviour {
+	public float moveSpeed;
+	public Rigidbody RB;
+
+	private Vector3 input;
+	// Use this for initialization
+	void Start () {
+		RB = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, Input.GetAxis ("Vertical"));
+		RB.AddForce(input*moveSpeed);
+		print (input);
+	}
+}
