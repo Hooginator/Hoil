1db3a48ce1a3686b5c484861a35a89b6ffd49a14
Eric PC <eric.hoog88@gmail.com>
2017-01-02 22:51:03 -0500

    Added brief start to Map classes

---
+++ Assets/Scripts/MapGridUnit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGridUnit : MonoBehaviour {
+	public float[] resources;// array of resource value of grid
+	public int uniqueResources; // Number of unique resources
+	public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		resources = new float[uniqueResources]; // set initial resources from outside I think is best.
+		transform.position = position;// Set position of Map unit
+		// WANT: Create Map tile game object from an Asset (I think)
+		// Change Colour?
+		// Add terrain that belongs to one map (Mountains, trees, buildings)
+		// Array of shit that impacts the resources (adjacency? over/under use?)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// WANT: reevaluate over/under use and alter resource value 
+		// Is it close to player? If not maybe destroy it until you're close again.
+	}
+}
