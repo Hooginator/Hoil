4a651b9cd3ea785a856d4a6dccce42ad13aca8b8
psigo <phillip.sigouin@gmail.com>
2017-01-08 20:25:40 -0500

    Add CharacterClass and Status Effect

---
+++ Assets/Scripts/StatusEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusEffect : MonoBehaviour {
+
+	class StatusEffect
+	{
+		public bool Bleed;
+		public bool Poison;
+		public bool Stun;
+		public bool Blind;
+		public bool Drunk;
+		public bool HeatStroke;
+		public bool FrostBite;
+
+
+}
