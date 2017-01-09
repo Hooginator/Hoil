712a23cba6f11c935cc0b88f0c1f5e88192816b6
psigo <phillip.sigouin@gmail.com>
2017-01-08 21:32:08 -0500

    Added crude attack function
    added in CharacterClass

--- Assets/Scripts/StatusEffect.cs
+++ Assets/Scripts/StatusEffect.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StatusEffect : MonoBehaviour {
-
-	class StatusEffect
-	{
-		public bool Bleed;
-		public bool Poison;
-		public bool Stun;
-		public bool Blind;
-		public bool Drunk;
-		public bool HeatStroke;
-		public bool FrostBite;
-
+	public bool Bleed;
+	public bool Poison;
+	public bool Stun;
+	public bool Blind;
+	public bool Drunk;
+	public bool HeatStroke;
+	public bool FrostBite;
 
 }
