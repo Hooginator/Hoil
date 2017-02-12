903adea8e9a2be8ebc7e180477fcd3541ddae061
Eric PC <eric.hoog88@gmail.com>
2017-02-11 20:34:53 -0500

    Comments and small changes
    Added a bunch of comments, commented out a bunch of old print tests and
    added a divide by zero protection.

--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -28,6 +28,7 @@ public class Team : MonoBehaviour {
 		//MeshRenderer rend = enemyPrefab.GetComponent<MeshRenderer> ();
 		//rend.sharedMaterial.SetColor ("_Color", colour);
 		armies.Clear();
+		// For some fucking reason this gets done BEFORE loading the main world, and so is in "battle", and so gets unloaded immediately.  even though I call it AFTER the load.  fuck you c#
 		spawnEnemies();
 	}
 	public void spawnEnemies(){
@@ -36,16 +37,21 @@ public class Team : MonoBehaviour {
 		}
 	}
 	void spawnEnemy(){
+		
 		print ("Spawning enenmy");
 		Vector3 basepos = mainBase.GetComponent<Transform> ().position;
 		minRange = 10f;
 		maxRange = 40f;
+		// Distance the enemy will spawn from the main base location
 		float dist = Random.Range (minRange, maxRange);
+		// angle from the base the enemy will spawn
 		float angle = Random.Range (0f, 2*Mathf.PI);
+		// Calculate teh Vector3 based on the RNG numbers
 		Vector3 relativeSpawn = new Vector3(dist*Mathf.Sin (angle),0,dist*Mathf.Cos (angle));
+		// Force the spawn location to fit inside the map.
 		var maaap = GameObject.Find ("Map");
 		Vector3 spawnPos =  maaap.GetComponent<Map> ().MirrorInsideBoundaries (relativeSpawn + basepos);
-		print ("Spawning Unit at:    " + spawnPos.ToString());
+		//print ("Spawning Unit at:    " + spawnPos.ToString());
 		GameObject tempArmy = GameObject.Instantiate (enemyPrefab, spawnPos, Quaternion.identity);
 		EnemyBehavior tempBehave = tempArmy.AddComponent<EnemyBehavior>();
 		tempBehave.moveSpeed = 0.5f;
