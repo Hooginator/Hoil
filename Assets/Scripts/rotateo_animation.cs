058c15086669f6256eeb2d41fbf222775c656678
psigo <phillip.sigouin@gmail.com>
2017-08-27 16:39:13 -0400

    animation changes

---
+++ Assets/Scripts/rotateo_animation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateo_animation : MonoBehaviour {
+	Transform TR;
+	ParticleSystem PS;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TR = gameObject.transform;
+		PS = gameObject.GetComponent<ParticleSystem> ();
+		var PSmain = PS.main.startRotation.constant;
+		PSmain = Time.frameCount;
+		TR.rotation = Quaternion.AngleAxis(Time.frameCount,new Vector3(1,0,0));
+	}
+}
